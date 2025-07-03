bd46ce4091fe1a0db4c372d87147eeff154628b4
mabin <220301120292@cutm.ac.in>
2025-07-03 15:52:13 +0530

    creating files under a folder and printing the file names

--- index.js
+++ index.js
@@ -30,17 +30,33 @@
 
 
 
-const fs=require('fs');
-const input=process.argv;
 
-if(input[2]=='add'){
-    fs.writeFileSync(input[3],input[4])
-}else if(input[2]=='remove'){
-    fs.unlinkSync(input[3]),
-    console.log("File deleted");
+// const fs=require('fs');
+// const input=process.argv;
+
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3],input[4])
+// }else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3]),
+//     console.log("File deleted");
     
-}
-else{
-    console.log("invalid input");
+// }
+// else{
+//     console.log("invalid input");
     
+// }
+
+
+
+
+
+const fs =require('fs');
+const path =require('path');
+const dirPath=path.join(__dirname,'files');
+
+for(i=1; i<5; i++){
+    fs.writeFileSync(dirPath+"/hello"+i+".txt","textfile no"+i);
 }
+fs.readdir(dirPath,(error,files)=>{
+    console.log(files);
+});
