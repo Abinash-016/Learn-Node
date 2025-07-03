f33cf8e593f94ee54b2e42d43878a5128e394805
mabin <220301120292@cutm.ac.in>
2025-07-03 15:01:24 +0530

    input from th ecommand prompt and creating and deleting files which are created or exists

--- index.js
+++ index.js
@@ -17,11 +17,30 @@
 // console.log(multiply(10,32));
 
 
-const data=require('./data')
-const http = require('http');
 
-http.createServer((req, resp) => {
-    resp.writeHead(200, { 'Content-type': 'application\json' });
-    resp.write(JSON.stringify(data));
-    resp.end();
-}).listen(5000);
+
+// const data=require('./data')
+// const http = require('http');
+
+// http.createServer((req, resp) => {
+//     resp.writeHead(200, { 'Content-type': 'application\json' });
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+// }).listen(5000);
+
+
+
+const fs=require('fs');
+const input=process.argv;
+
+if(input[2]=='add'){
+    fs.writeFileSync(input[3],input[4])
+}else if(input[2]=='remove'){
+    fs.unlinkSync(input[3]),
+    console.log("File deleted");
+    
+}
+else{
+    console.log("invalid input");
+    
+}
