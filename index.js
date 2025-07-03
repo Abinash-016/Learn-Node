08e48e449a7973535571323af760816dd802c5da
mabin <220301120292@cutm.ac.in>
2025-07-03 10:18:10 +0530

    added package.json file and created a server.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const fs=require('fs');
+// const fs=require('fs');
 
 // const x=20
 // console.log(x)
 
-fs.writeFileSync("Hello.txt", "Abinash")
+// fs.writeFileSync("Hello.txt", "Abinash")
+
+
+
+
+const http =require('http');
+
+http.createServer((req, resp)=>{
+    resp.write("Hello User");
+    resp.end();
+}).listen(5000);
