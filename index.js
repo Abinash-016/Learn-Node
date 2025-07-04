611fc4a1f641e28a5319306d723ea25d74808873
mabin <220301120292@cutm.ac.in>
2025-07-04 20:50:30 +0530

    handling asynchronous data and sending response through express js

--- index.js
+++ index.js
@@ -84,3 +84,21 @@
 
 
 // fs.unlinkSync(`${filepath}`); //deleted the file 
+
+
+
+
+//                                       How to handle asynchronous data 
+let a=10;
+let b=5;
+
+const waitingData= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(20)
+    },2000)
+})
+
+waitingData.then((data)=>{
+    b=data;
+    console.log(a+b);
+})
