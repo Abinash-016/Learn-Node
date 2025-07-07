5a7c0bb6fbf8cd0789be193f2ea24fab3a634e33
mabin <220301120292@cutm.ac.in>
2025-07-07 15:08:20 +0530

    introduction to midddleware

--- express.js
+++ express.js
@@ -1,21 +1,37 @@
 const express=require('express');
-const path=require('path');
+// const path=require('path');
 const app=express();
 
-const htmlPath=path.join(__dirname,'Htmlpages')
-
-// app.get('', (req,resp)=>{
-//     resp.send("Hello, this is home page")
-// });
-
-// app.get('/about', (req,resp)=>{
-//     resp.send("Hello, this is About page")
-// });
+// const htmlPath=path.join(__dirname,'Htmlpages')
 
 
+const reqFilter=(req,resp,next)=>{
+    if(!req.query.age){
+        resp.send("Please provide your age")
+    }
+    else if(req.query.age<18){
+        resp.send("Your age is below 18 You cannot access this page.....")
+    }
+     else{
+         next();
+    }
+   
+}
+
+app.use(reqFilter);
+
+app.get('', (req,resp)=>{
+    resp.send("Hello, this is home page")
+});
+
+app.get('/about', (req,resp)=>{
+    resp.send("Hello, this is About page")
+});
 
 
-app.use(express.static(htmlPath));
 
-app.listen(5000);
+
+// app.use(express.static(htmlPath));
+
+app.listen(5001);
 
