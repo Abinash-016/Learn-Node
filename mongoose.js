c29087bdd63ffe6a97517cdfc4820ae0dce1174e
mabin <220301120292@cutm.ac.in>
2025-07-16 14:52:46 +0530

    update and delete method

--- mongoose.js
+++ mongoose.js
@@ -5,28 +5,40 @@ const mongoose=require('mongoose');
         price:Number
     });
 
-const saveInDb = async ()=>{
-    await mongoose.connect("mongodb://localhost:27017/Flipkart");
+// const saveInDb = async ()=>{
+    // await mongoose.connect("mongodb://localhost:27017/Flipkart");
  
 
-    const ProductModel=mongoose.model('products', ProductSchema);
-        let data=new ProductModel({name:"13 s", price:40000});
-        let result=await data.save();
-        console.log(result);
+//     const ProductModel=mongoose.model('products', ProductSchema);
+//         let data=new ProductModel({name:"13 s", price:40000});
+//         let result=await data.save();
+//         console.log(result);
         
-}
+// }
 
-saveInDb();
+// saveInDb();
 
 
 const updateInDb = async ()=>{
+    await mongoose.connect("mongodb://localhost:27017/Flipkart");
+
      const ProductModel=mongoose.model('products', ProductSchema);
      let data= await ProductModel.updateOne(
         {name:"13 s"},
         {$set:{
-            price:49000
+            price:39900
         }}
 
      )
 }
 updateInDb();
+
+
+
+const deleteInDb= async ()=>{
+
+    const ProductModel=mongoose.model('products', ProductSchema);
+    let data= await ProductModel.deleteOne({name:"Reno 10 5G"});
+}
+
+deleteInDb();
